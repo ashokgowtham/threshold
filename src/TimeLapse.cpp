8e7ff83d4d0809124c622e2e95ed26ca654b7c0d
Ashok Gowtham M <ashokgowtham123@gmail.com>
2015-05-13 16:08:26 +0530

    Forked repo into Threshold

--- src/TimeLapse.cpp
+++ src/TimeLapse.cpp
@@ -9,63 +9,64 @@ int DEFAULT_TIME_INTERVAL = 1;
 int DEFAULT_CAMERA = 0;
 
 using namespace cv;
-void writeOutputImage(Mat& image, char *folder, int fileNumber) {
+void writeOutputImage(Mat& image, char *src_file) {
     char path[1024] = {};
-    sprintf(path, "%s/image%04d.jpg", folder, fileNumber);
+    sprintf(path, "%s.thresholded.png", src_file);
     imwrite(path, image );
 }
 
 int main( int argc, char** argv )
 {
-
-    char *outputFolder = DEFAULT_OUTPUT_FODLER;
-    int timeInterval = DEFAULT_TIME_INTERVAL;
-    int cameraId = DEFAULT_CAMERA;
-    int currentFileCount = 1;
-    bool writeVideo = false;
-    int outputVideoFps = 24;
-
-    printf("Usage: <[image]|video> <outputFolder[\".\"]> <timeInterval[1]> <camera[0]>\nRunning with defaults...\n");
-
-    if(argc>1) writeVideo = strcmp(argv[1],"video")==0;
-    if(argc>2) outputFolder = argv[2];
-    if(argc>3) sscanf(argv[3], "%d", &timeInterval);
-    if(argc>4) sscanf(argv[4], "%d", &cameraId);
-
-
-    Mat frame;
-    //open input
-    VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
+    if( argc != 2)
+    {
+        printf(" Usage: threshold ImageToThreshold\n");
         return -1;
+    }
 
-    //open output
-    VideoWriter outputVideo;    // Open the output
-    if(writeVideo)
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+
+    if(! image.data )                              // Check for invalid input
     {
-        cap >> frame;               // get a new frame from camera to determine output video size
-        outputVideo.open("./videoFile.mp4", -1, outputVideoFps, frame.size(), true);
-        if (!outputVideo.isOpened())
-        {
-            printf("Could not open the output video for write.\n");
-            return -1;
-        }
+        printf("Could not open or find the image\n");
+        return -1;
     }
-    namedWindow("preview", CV_WINDOW_AUTOSIZE);
-    for(;;)
-    {
-        cap >> frame; // get a new frame from camera
-        imshow("preview", frame);
-        if (writeVideo)
-        {
-            outputVideo << frame;
-        }
-        else
-        {
-            writeOutputImage(frame, outputFolder, currentFileCount++);
-        }
-        if(waitKey(27) >= 0) break;
-        sleep(timeInterval);
-    }
+
+    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+    waitKey(0);                                          // Wait for a keystroke in the window
+    
+    Mat gray_image;
+    cvtColor( image, gray_image, CV_BGR2GRAY );
+    imshow( "Display window", gray_image );                   // Show our image inside it.
+    waitKey(0);                                          // Wait for a keystroke in the window
+
+    Mat hist_image;
+    equalizeHist( gray_image, hist_image );
+    imshow( "Display window", hist_image );                   // Show our image inside it.
+    waitKey(0);                                          // Wait for a keystroke in the window
+
+    Mat thresholded_image;
+    Mat adaptive_thresholded_image;
+    Mat binary_image;
+    adaptiveThreshold(gray_image,adaptive_thresholded_image,255,ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY_INV,151,30);
+    threshold(hist_image,thresholded_image,50,255,THRESH_BINARY_INV);
+
+    imshow( "Display window", thresholded_image );                   // Show our image inside it.
+    waitKey(0);                                          // Wait for a keystroke in the window
+
+    imshow( "Display window", adaptive_thresholded_image );                   // Show our image inside it.
+    waitKey(0);                                          // Wait for a keystroke in the window
+
+    bitwise_and(thresholded_image,adaptive_thresholded_image,binary_image);
+    imshow( "Display window", binary_image );                   // Show our image inside it.
+    waitKey(0);                                          // Wait for a keystroke in the window
+
+    bitwise_not(binary_image,binary_image);
+    imshow( "Display window", binary_image );                   // Show our image inside it.
+    waitKey(0);                                          // Wait for a keystroke in the window
+
+    writeOutputImage(binary_image,argv[1]);
+
     return 0;
 }
